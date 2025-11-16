eaaa7961392d148a821129fa0b302fb1ae25269a
Ergenekon Yigit <yigitergenekon@gmail.com>
2025-11-17 00:33:25 +0300

    Fix formatting in keymap layers for consistency

--- keyboards/crkbd/rev1/keymaps/ergenekonyigit/keymap.c
+++ keyboards/crkbd/rev1/keymaps/ergenekonyigit/keymap.c
@@ -125,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TILD,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,     KC_CIRC,  KC_AMPR,  KC_PAST,  KC_LPRN,  KC_RPRN,  KC_BSPC,
       KC_LCTL,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,     KC_PMNS,  KC_EQL,   KC_GRV,   KC_LBRC,  KC_RBRC,  KC_BSLS,
       KC_LSFT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,     KC_UNDS,  KC_PPLS,  KC_TILD,  KC_LCBR,  KC_RCBR,  KC_PIPE,
-                                    KC_LALT,  KC_LGUI,  KC_SPC,      XXXXXXX,  _______,  MO(_FUNC),
+                                    KC_LALT,  KC_LGUI,  KC_SPC,      XXXXXXX,  _______,  MO(_FUNC)
   ),
 
 /* number layer
@@ -144,7 +144,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,   KC_1,  KC_2,  KC_3,     KC_4,     KC_5,        KC_6,     KC_7,         KC_8,    KC_9,    KC_0,    KC_BSPC,
       KC_LCTL,  KC_4,  KC_5,  KC_6,     XXXXXXX,  XXXXXXX,     KC_F1,    KC_F2,        KC_F3,   KC_F4,   KC_F5,   KC_F6,
       KC_LSFT,  KC_7,  KC_8,  KC_9,     KC_0,     XXXXXXX,     KC_F7,    KC_F8,        KC_F9,   KC_F10,  KC_F11,  KC_F12,
-                              KC_LALT,  KC_LGUI,  KC_SPC,      XXXXXXX,  MO(_FUNC),  _______,
+                              KC_LALT,  KC_LGUI,  KC_SPC,      XXXXXXX,  MO(_FUNC),  _______
   ),
 
 /* function layer
